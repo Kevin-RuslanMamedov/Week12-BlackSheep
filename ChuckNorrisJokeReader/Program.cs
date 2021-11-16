3f02c44a34857b6b957d69db76b6b8517226789e
Opilane TTHK <opilane@tthk.ee>
2021-11-16 16:21:02 +0200

    week12

---
+++ ChuckNorrisJokeReader/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorrisJokeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of jokes");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            GetJokes(userNumber);
+
+        }
+
+        private static void GetJokes(int jokesNumber)
+        {
+            string filePath = @"C:\Users\opilane\Samples\chuck.txt";
+            string[] jokesAboutChuck = File.ReadAllLines(filePath);
+
+            if(jokesNumber > jokesAboutChuck.Length)
+            {
+                jokesNumber = jokesAboutChuck.Length;
+            }
+
+            for(int i = 0; i < jokesNumber; i++)
+            {
+                Console.WriteLine(jokesAboutChuck[i]);
+            }
+
+        }
+    }
+}
---
+++ FruitAndVegetables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace FruitAndVegetables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read data from fruit.txt
+            string fruitPath = @"C:\Users\opilane\Samples\fruit.txt";
+            string[] veggieData = File.ReadAllLines(fruitPath);
+
+            //read data from veggies.txt
+            string veggiePath = @"C:\Users\opilane\Samples\veggie.txt";
+            string[] fruitData = File.ReadAllLines(veggiePath);
+
+            File.WriteAllLines(fruitPath, fruitData);
+            File.WriteAllLines(veggiePath, veggieData);
+
+
+
+        }
+    }
+}
